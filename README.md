7285cc2464f22a5be252e0605e954c5e432f04b1
Gatea David <davidgatea21@gmail.com>
2023-10-23 01:08:06 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+Read Me file for Assembly extension for ollydbg
+
+# Assembly for OllyDbg
+
+This extension adds support for assembly language to OllyDbg.
+
+
+
+## Features
+
+* Debugging assembly code in OllyDbg.
+
+
+## Requirements
+
+Asm Tools for OllyDbg is required to be downloaded. It can be downloaded from [here](https://www.cs.ubbcluj.ro/~vancea/asc/files/ASM_tools.zip)
+
+## Extension Settings
+
+TO DO
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+TO DO
+
+
+## Following extension guidelines
+
+Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
+
+* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,204 @@
+import * as vscode from 'vscode';
+import {exec} from 'child_process';
+
+const diagnosticCollection = vscode.languages.createDiagnosticCollection('asm');
+
+let activeTextEditor = vscode.window.activeTextEditor;
+let currentDebugFileExecutable = '';
+let stoppingExecutable = false;
+
+const updateConfig = (editor: any) => {
+
+    const isAsmFile:boolean = editor && editor.document.fileName.endsWith('.asm');
+
+    vscode.workspace.getConfiguration().update('asm.showRunIconInEditorTitleMenu', isAsmFile, vscode.ConfigurationTarget.Global);
+};
+
+
+export function activate(context: vscode.ExtensionContext) {
+
+	const onChangeTextEditor = vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeTextEditor = editor;
+		updateConfig(editor);
+	});
+	const getExecutablePath = async () => {
+		var executablePath = vscode.workspace.getConfiguration().get('asm.executablePath');
+
+		if (executablePath === undefined || executablePath === '') {
+			executablePath = await vscode.window.showInputBox(
+				{
+					placeHolder: 'Enter executable path',
+					value: ""
+				}
+			);
+			vscode.workspace.getConfiguration().update('asm.executablePath', executablePath, vscode.ConfigurationTarget.Global);
+			vscode.window.showInformationMessage(`Saved executable path: ${executablePath}`);
+			
+		} 
+		return executablePath;
+	};
+	
+	const setRunningState = (isRunning: boolean) => {
+		vscode.workspace.getConfiguration().update('asm.showStopIconInEditorTitleMenu', isRunning, vscode.ConfigurationTarget.Global);
+	};
+
+	const isTerminalVisible = async () => {
+		return vscode.workspace.getConfiguration().get('asm.showTerminalInDebugMode');
+	};
+
+	const stopDebugging = async () => {
+		const executableName = 'ollydbg.exe';
+		stoppingExecutable = true;
+		await killExecutable(executableName);
+		if (currentDebugFileExecutable !== undefined && currentDebugFileExecutable !== '') {
+			await killExecutable(currentDebugFileExecutable!);
+		}
+		setRunningState(false);
+		stoppingExecutable = false;
+	};
+
+	async function killExecutable(executable: string): Promise<string>
+	{
+		return new Promise<string>((resolve, reject) => {
+			exec('tasklist', (error, stdout) => {
+				if (error) {
+				  console.error(`Error checking for running processes: ${error}`);
+				  reject(error.message);
+				}
+				if (stdout.toLowerCase().includes(executable.toLowerCase())) {
+					console.log(`The executable ${executable} is running. Stopping it...`);
+					exec(`taskkill /F /IM ${executable}`, (error, stdout) => {
+						if (error) {
+						  console.error(`Error stopping ${executable}: ${error}`);
+						  resolve("error");
+						}
+						console.log(`${executable} has been successfully stopped.`);
+						resolve(stdout);
+					  });
+				} else {
+					console.log(`The executable ${executable} is not running.`);
+					resolve(stdout);
+				}
+			});
+		});
+	}
+	
+
+
+	async function startExecutable(executable: string): Promise<string> {
+
+		const documentFileName = activeTextEditor?.document.fileName;
+		return new Promise<string>((resolve, reject) => {
+		  exec(`${executable}`, (error, stdout) => {
+			if (error && !stoppingExecutable) {
+			  console.error(`Error starting ${executable}: ${error}`);
+			  
+			  const splitError = error.message.split('\n');
+			  if (splitError.length > 1) {
+				const pattern = /:(\d+):/;
+				const match = splitError[1].match(pattern);
+
+				if (match) {
+					const lineNumber = match[1];
+					const range = new vscode.Range(new vscode.Position(Number(lineNumber)-1, 0), new vscode.Position(Number(lineNumber)-1, 1000));
+					const diagnostic = new vscode.Diagnostic(range, splitError[1], vscode.DiagnosticSeverity.Error);
+					const diagnostics: vscode.Diagnostic[] = [];
+					diagnostics.push(diagnostic);
+					diagnosticCollection.set(vscode.Uri.file(documentFileName!), diagnostics);
+
+
+
+				  console.log("Line Number:", lineNumber);
+				} 
+				else {
+				  console.log("Line number not found in the error message.");
+				}
+
+				reject(`Errors: ${splitError[1]}`);
+			  }
+			  else {
+				reject(`Error: ${error.message}`);
+			  }
+			}
+			console.log(`${executable} has been successfully started and terminal minimized.`);
+			resolve(stdout);
+		  });
+		});
+	}
+
+
+	function deleteFile(fileName: string) {
+		const fs = require('fs');
+		console.log(`Deleting file ${fileName}`);
+		fs.unlink(fileName, (err: any) => {
+			if (err) {
+				console.error(err);
+				return err;
+			}
+		});
+	}
+
+
+	const run = vscode.commands.registerCommand('asm.run', async () => {
+		await stopDebugging();
+		var executablePath = await getExecutablePath();
+		console.log(executablePath);
+		if (executablePath === undefined || executablePath === '') {
+			vscode.window.showInformationMessage('No executable path set!');
+			return;
+		}
+		if (activeTextEditor === undefined) {
+			vscode.window.showInformationMessage('No active editor!');
+			return;
+		}
+		const currentFile = activeTextEditor.document.fileName;
+
+		var showTerminal = await isTerminalVisible();
+
+		const lstFile = currentFile.slice(0, -3) + 'lst';
+		const objFile = currentFile.slice(0, -3) + 'obj';
+		const exeFile = currentFile.slice(0, -3) + 'exe';
+		const nasmCommand = `"${executablePath}"\\nasm\\nasm.exe -fobj "${currentFile}" -l "${lstFile}" -I"${executablePath}"\\nasm\\\\`;
+		
+		var alinkCommand = "";
+		if (showTerminal) {
+			alinkCommand = `"${executablePath}\\nasm\\ALINK.EXE" -oPE -subsys console -entry start "${objFile}"`;
+		}
+		else {
+			alinkCommand = `"${executablePath}\\nasm\\ALINK.EXE" -oPE -entry start "${objFile}"`;
+		}
+		const ollydbgCommand = `"${executablePath}\\ollydbg\\ollydbg.exe" "${exeFile}"`;
+
+		var fileExecutableName:string|undefined = currentFile.slice(0, -3) + 'exe';
+		fileExecutableName = fileExecutableName.split('\\').pop();
+
+		currentDebugFileExecutable = fileExecutableName!;
+
+		diagnosticCollection.clear();
+
+
+		await startExecutable(nasmCommand);
+		await startExecutable(alinkCommand);
+
+		setRunningState(true);
+		await startExecutable(ollydbgCommand);
+		setRunningState(false);
+
+
+
+		deleteFile(lstFile);
+		deleteFile(objFile);
+		deleteFile(exeFile);
+
+	});
+
+	const stop = vscode.commands.registerCommand('asm.stop', async () => {
+		await stopDebugging();
+	});
+
+	context.subscriptions.push(run);
+	context.subscriptions.push(stop);
+	context.subscriptions.push(onChangeTextEditor);
+}
+
+export function deactivate() {}
